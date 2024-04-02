16d2fcc547a5b18e476f0c777729e43292a61bfc
Vikas Kashyap <126238976+vikaskashyap7@users.noreply.github.com>
2024-04-02 09:35:11 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,12 +12,12 @@ function HeroSection() {
   return (
       <div className=' w-[375px] h-fit  mx-auto  relative '>
         <div className='w-[344px] h-[344px]  absolute top-[249px] left-[16px] '>
-          <Image src={Img4} alt'Img4' />
-          <Image src={Img3} alt='Img3' className=' absolute top-[35px] left-[31px]'/>
-          <Image src={Img2} alt='Img2' className=' absolute top-[64px] left-[58px]'/>
-          <Image src={Img1} alt='img1' className=' absolute top-[98px] left-[88px]'/>
-          <Image src={circle} alt='circle' className=' absolute top-[128px] left-[117px]'/>
-          <Image src={Bell} alt='bell' className=' absolute top-[146px] left-[135px]'/>
+          <Image src={Img4} alt="Img4" />
+          <Image src={Img3} alt="Img3" className=' absolute top-[35px] left-[31px]'/>
+          <Image src={Img2} alt="Img2" className=' absolute top-[64px] left-[58px]'/>
+          <Image src={Img1} alt="img1" className=' absolute top-[98px] left-[88px]'/>
+          <Image src={circle} alt="circle" className=' absolute top-[128px] left-[117px]'/>
+          <Image src={Bell} alt="bell" className=' absolute top-[146px] left-[135px]'/>
         </div>
         
       </div>
